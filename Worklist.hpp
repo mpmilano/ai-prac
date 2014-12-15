a45bdaf975d2a55dd1c66b0890b263a87136991b
Matthew Milano <milano@cs.cornell.edu>
2014-12-15 14:24:24 -0500

    fixing astar, maybe

--- Worklist.hpp
+++ Worklist.hpp
@@ -1,21 +1,27 @@
-template<typename T, typename U = T>
+template<typename T, typename U = T, typename V = T>
 class PriorityWorklist {
 	std::set<U> seen;
-	std::priority_queue<T> todo;
+	std::set<T> holding;
+	std::priority_queue<V> todo;
 	std::function<U (const T&) > track;
+	std::function<V (const T&) > pair;
+	std::function<const T& (const V&) > depair;
 public:
-	PriorityWorklist(decltype(track) track):track(track){}
+	PriorityWorklist(decltype(track) track, decltype(pair) pair, decltype(depair) depair)
+		:track(track),pair(pair),depair(depair){}
 
 	void push(const T& e){
 		if (seen.find(track(e)) == seen.end()) {
-			todo.push(e);
+			holding.insert(e);
+			todo.push(pair(*holding.find(e)));
 			seen.insert(track(e));
 		}
 	}
 
 	const T& top(){
-		const auto &ret = todo.top();
+		const auto &ret = depair(todo.top());
 		todo.pop();
+		holding.erase(ret);
 		return ret;
 	}
 
@@ -23,12 +29,17 @@ public:
 
 };
 
-template<typename T, typename U>
-PriorityWorklist<T,U> build_pwl(const std::function<U (const T&) > &track){
-	return PriorityWorklist<T,U>(track);
+template<typename T, typename U, typename V>
+auto build_pwl(const std::function<U (const T&) > &track,
+			   const std::function<V (const T&) > &pair,
+			   const std::function<const T& (const V&) > &depair){
+	return PriorityWorklist<T,U,V>(track,pair,depair);
 }
 
-template<typename T, typename U>
-PriorityWorklist<T,U> build_pwl(U (*track) (const T&)){
-	return PriorityWorklist<T,U>(track);
+template<typename T, typename U, typename V>
+auto build_pwl(U (*track) (const T&),
+			   V (*pair) (const T&),
+			   const T& (*depair) (const V&) ){
+	PriorityWorklist<T,U,V> ret(track,pair,depair);
+	return ret;
 }
--- astar.cpp
+++ astar.cpp
@@ -8,32 +8,55 @@ struct path {
 	enum class empty { EMPTY};
 	const Node& elem;
 	const boost::variant<empty, const std::shared_ptr<path> > r;
-	double weight;
-	path(const Node& elem, const boost::variant<empty, const std::shared_ptr<path> > r, std::function<double (const Node&)> cost)
-		:elem(elem),r(r),weight(match(r,
-									  [](empty) -> double {return 0;},
-									  [](const std::shared_ptr<path> r) -> double {return r->weight;})
-								+ cost(elem)){}
+	const double weight;
+	const double estimate;
+	path(const Node& elem,
+		 //previous
+		 const boost::variant<empty, const std::shared_ptr<path> > r,
+		 //actual cost of taking just this hop in the path;
+		 double weight,
+		 //cost of taking imaginary straight-shot from
+		 //hop end to goal.
+		 std::function<double (const Node&)> cost)
+		:elem(elem),r(r),
+		 weight(weight + match(r,
+							   [](empty) -> double {return 0;},
+							   [](const std::shared_ptr<path> r) -> double {return r->weight;}))
+		,estimate(cost(elem)){}
+
 
 	bool operator<(const path &p) const {
-		return weight < p.weight;
+		return weight + estimate < p.weight + p.estimate;
 	}
 	bool operator==(const path &p) const{
-		return weight == p.weight;
+		return (&elem == &p.elem) && r == p.r;
 	}
+
+	double priority(){ return weight + estimate; }
 
 	path (const path& path) = delete;
 	
 };
 
-#define extend(el,p) std::shared_ptr<path>(new path(el,p,h));
+//TODO: assuming all paths have equal costs for some reason.
+#define extend(el,p) std::shared_ptr<path>(new path(el,p,1,h));
 
 Node_cp get_head(const std::shared_ptr<path>& p) {
 	return p->elem.self();
 }
+
+auto pair_with_cost(const std::shared_ptr<path> &p){
+	return std::make_pair(p->priority(), &p);
+}
+
+const auto& depair(const std::pair<double, const std::shared_ptr<path>* > &p){
+	return *p.second;
+}
+
+//TODO: this is all broken because of the priority queue :( 
 std::shared_ptr<path> a_star(const Node &first, const Node& goal,
 							 std::function<double (const Node& )> h){
-	auto open = build_pwl(get_head);
+	auto open = build_pwl(get_head, pair_with_cost, depair);
 	auto init = extend(first,path::empty::EMPTY);
 	open.push(init);
 	
