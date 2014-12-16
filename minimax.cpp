5564717ee4e88310152ec3d305b0248f862c9787
Matthew Milano <milano@cs.cornell.edu>
2014-12-16 01:55:05 -0500

    mostly cosmetic minimax changes

--- minimax.cpp
+++ minimax.cpp
@@ -3,12 +3,17 @@
 #include <functional>
 #include "path.hpp"
 
-template<typename Vec, typename Node, typename L>
-auto minimax(const Node& a,
-			 const std::function<int (decltype(a)) > &utility,
-			 const std::function<bool (decltype(a), const Vec&, const Vec& )> &compare,
-			 const L &children_f){
+//might want to consider memoizing this. 
+template<typename F, typename G, typename Node, typename L, typename Vec>
+std::pair<Vec, path<Node> > minimax(const Node& a, const F &utility_f, const G &compare_f, const L &children_f,
+			 std::function<decltype(a) (decltype(a))> parent, Vec& alphabeta){
+	//typdef decltype(utility(a)) Vec;
 	const std::function<decltype(children_f(a)) (decltype(a)) > &children = children_f;
+	const std::function<Vec (decltype(a)) > &utility = utility_f;
+	const std::function<bool (decltype(a), const Vec&, const Vec& )> &compare = compare_f;
+		
+	using namespace std::placeholders;
+	auto minimax_r = std::bind(minimax<decltype(utility), decltype(compare), Node, decltype(children), Vec>, _1, utility, compare, children, parent, _2);
 
 	Vec wantmax;
 	path<Node> statemax(a,path<Node>::empty::EMPTY);
@@ -17,11 +22,17 @@ auto minimax(const Node& a,
 		return std::make_pair(utility(a),statemax);
 	}
 	else {
+		Vec oldab = alphabeta;
 		for (const auto &c : kids){
-			auto r = minimax(c, utility, compare, children);
+			auto r = minimax_r(c, alphabeta);
 			if (compare(a,r.first,wantmax) ) {
+				alphabeta = oldab;
 				wantmax = r.first;
 				statemax = path<Node>(c, r.second);
+				if (compare(parent(a), r.first, alphabeta)) {
+					oldab = alphabeta;
+					alphabeta = r.first;
+				} else break;
 			}
 		}
 		return std::make_pair(wantmax, statemax);
@@ -29,8 +40,9 @@ auto minimax(const Node& a,
 }
 
 int main(){
-	
-	minimax<int>(0, [](const int&){return 0;},
+
+	static int alphabeta = 0;
+	minimax(0, [](const int&){return 0;},
 			[](const int&, const int &f, const int& o){return f < o; },
-			[](const int&){return std::list<int>(); });
+				 [](const int&){return std::list<int>(); }, [&](const int &) -> const int& {return alphabeta;}, alphabeta);
 }
